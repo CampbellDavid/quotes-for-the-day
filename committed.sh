aa31d558b0e5d73e2c504bc641da851e1f1761a8
CampbellDavid <dcampbell092@gmail.com>
2020-12-04 11:40:00 +0000

    Them as has, gets.

--- committed.sh
+++ committed.sh
@@ -3,8 +3,8 @@ NUMBER=$(($RANDOM % 5))
 
 for ((run=1; run <= NUMBER + 1; run++))
 do
-  echo `fortune` > ~/pusher/file.txt
+  echo `/usr/local/bin/fortune` > ~/pusher/file.txt
   git -C ~/pusher/ add . -A
-  git -C ~/pusher/ commit -m "`fortune -sn 32`"
+  git -C ~/pusher/ commit -m "`/usr/local/bin/fortune -sn 32`"
 done
 
